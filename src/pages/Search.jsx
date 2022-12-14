385c20f31ef64b28796ef0eb4e7670082a2020de
joseg-alvesg <cinhoalves85@gmail.com>
2022-12-14 14:06:29 -0300

    correção do requisito 04, não estava sendo possivel digitar no campo do input, correção na quantidade de caracteres

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,11 +2,48 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 export default class Search extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      search: '', // chave do input de busca
+      btnDisable: true, // chave do atributo disabled
+    };
+  }
+
+  handleChange = ({ target: { value } }) => {
+    // definição e captura dos valores atraves do estado do componente
+    this.setState({ search: value }, this.verify);
+  };
+
+  verify = () => {
+    const { search } = this.state;
+    // validação do botão de busca, sendo necessario ter mais de dois caracteres para poder realizar uma pesquisa
+    this.setState({ btnDisable: search.length < 2 });
+  };
+
   render() {
+    const { search, btnDisable } = this.state;
     return (
       <div data-testid="page-search">
-        Search
         <Header />
+        Search
+        <div>
+          <input
+            type="text"
+            data-testid="search-artist-input"
+            name="search"
+            value={ search }
+            onChange={ this.handleChange }
+          />
+          <button
+            type="button"
+            disabled={ btnDisable }
+            data-testid="search-artist-button"
+          >
+            Pesquisar
+          </button>
+        </div>
       </div>
     );
   }
