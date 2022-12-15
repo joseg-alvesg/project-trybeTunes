4f959caa4370a1f0068e8ccdd8ca292bc63a7a2a
joseg-alvesg <cinhoalves85@gmail.com>
2022-12-15 17:33:00 -0300

    comentatios nas linhas do arquivo Album.jsx

--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -17,23 +17,23 @@ class Album extends Component {
     super(props);
 
     this.state = {
-      name: '',
-      albumName: '',
-      albumInfos: [],
+      name: '', // para o nome do artista ou banda
+      albumName: '', // para o nome do album
+      albumInfos: [], // para salvar as informações retornadas da API
     };
   }
 
   componentDidMount() {
-    this.handleApi();
+    this.handleApi(); // renderiza o componente assim que o componente é montado
   }
 
   handleApi = async () => {
-    const { match: { params: { id } } } = this.props;
-    const request = await getMusics(id);
+    const { match: { params: { id } } } = this.props; // o Id através das rotas do react router que é enviado através do link na linhas 20 do componente Card.jsx
+    const request = await getMusics(id); // aqui é feita a requisição para o eme
     console.log(request);
-    this.setState({ name: request[0].artistName,
+    this.setState({ name: request[0].artistName, // captura as informações do nome da banda/artista e do nome do album que foi clicado
       albumName: request[0].collectionName,
-      albumInfos: request });
+      albumInfos: request }); // envia todo o objeto do recebido da api para um array no estado
   };
 
   render() {
@@ -47,7 +47,7 @@ class Album extends Component {
           <h2 data-testid="album-name">{albumName}</h2>
         </div>
         {albumInfos
-          .filter((elem) => elem.trackName !== undefined)
+          .filter((elem) => elem.trackName !== undefined) // filta todos os objetos do array que não possuem uma informação de musica, para renderizar a penas as musicas daqui pra baixo
           .map((elem) => (
             <MusicCard
               key={ elem.trackId }
