4ddde8d29eedc7b456bb62c2e7e3cb2e1cb3fa6f
joseg-alvesg <cinhoalves85@gmail.com>
2022-12-17 13:49:20 -0300

    Requisito 13 - inicio da criação da logica itilizando a requisição das informações de usuario

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,12 +1,53 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
+import { getUser } from '../services/userAPI';
 
 export default class Profile extends Component {
+  state = {
+    name: '',
+    email: '',
+    description: '',
+    image: '',
+  }
+
+  componentDidMount() {
+    this.getAccountInfos();
+  }
+
+  getAccountInfos = async () => {
+    const accountRequest = await getUser();
+    console.log(accountRequest);
+    // this.setState({ name: });
+  };
+
   render() {
+    const { name } = this.state;
     return (
       <div data-testid="page-profile">
-        Profile
         <Header />
+        <div>
+          <img src="" alt="" />
+        </div>
+        <ul>
+          <li>
+            <h4>
+              Nome
+            </h4>
+            <p>{name}</p>
+          </li>
+          {/* <li>
+            <h4>
+              Email
+            </h4>
+            <p>a</p>
+          </li>
+          <li>
+            <h4>
+              Descrição
+            </h4>
+            <p>a</p>
+          </li> */}
+        </ul>
       </div>
     );
   }
